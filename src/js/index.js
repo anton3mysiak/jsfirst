43760c7dfbeca8ddfe4aff74798a2f2e92f8d1de
AntonMysiak <anton.mysyack9@gmail.com>
2024-02-02 20:03:13 +0100

    variables_types

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// let num = 3;
+// let element;
+// element = 'Body'
+// alert(num);
+// window.alert(element);
+// var 
+
+// const a = 3000;
+// const b = 5000;
+// console.log()
+
+let userName = 'John';
+let userNumber = 25;
+userNumber = 24;
+console.log(userName);
+console.log(userNumber);
