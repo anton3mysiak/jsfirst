e314956ab44cc7eb70b2d35e4f2e76f0c372a061
AntonMysiak <anton.mysyack9@gmail.com>
2024-02-06 13:27:55 +0100

    types_of_data

--- src/js/index.js
+++ src/js/index.js
@@ -16,3 +16,36 @@ let userNumber = 25;
 userNumber = 24;
 console.log(userName);
 console.log(userNumber);
+
+let firstNum = 4.4;
+// console.log(number + 5);
+
+typeof(number);
+console.log(typeof(firstNum));
+console.log(typeof("Hello"));
+
+let und;
+console.log(typeof(und));
+
+const man = { //object
+    name: 'Anton',
+    age: 26,
+    isMarried: false
+};
+console.log(man.age);
+
+let arr = ['Valeria', 1.5, false]
+console.log(arr[1]);
+
+let storeName = 'Rozetka';
+const storeDescription = {
+    budget: 10000,
+    employees: ['Anton', 'Valeria', 'Alex'],
+    products: {
+        laptop: 100,
+        mouse: 30
+    },
+    isOpen: true
+}
+
+console.log(storeDescription.employees[1])
