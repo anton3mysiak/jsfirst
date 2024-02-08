e6b997470f0f04083cc7a8513ac52ed15b856548
AntonMysiak <anton.mysyack9@gmail.com>
2024-02-08 22:48:34 +0100

    operators

--- src/js/index.js
+++ src/js/index.js
@@ -11,41 +11,76 @@
 // const b = 5000;
 // console.log()
 
-let userName = 'John';
-let userNumber = 25;
-userNumber = 24;
-console.log(userName);
-console.log(userNumber);
+// let userName = 'John';
+// let userNumber = 25;
+// userNumber = 24;
+// console.log(userName);
+// console.log(userNumber);
 
-let firstNum = 4.4;
-// console.log(number + 5);
+// let firstNum = 4.4;
+// // console.log(number + 5);
 
-typeof(number);
-console.log(typeof(firstNum));
-console.log(typeof("Hello"));
+// typeof(number);
+// console.log(typeof(firstNum));
+// console.log(typeof("Hello"));
 
-let und;
-console.log(typeof(und));
+// let und;
+// console.log(typeof(und));
 
-const man = { //object
-    name: 'Anton',
-    age: 26,
-    isMarried: false
-};
-console.log(man.age);
+// const man = { //object
+//     name: 'Anton',
+//     age: 26,
+//     isMarried: false
+// };
+// console.log(man.age);
 
-let arr = ['Valeria', 1.5, false]
-console.log(arr[1]);
+// let arr = ['Valeria', 1.5, false]
+// console.log(arr[1]);
 
-let storeName = 'Rozetka';
-const storeDescription = {
-    budget: 10000,
-    employees: ['Anton', 'Valeria', 'Alex'],
-    products: {
-        laptop: 100,
-        mouse: 30
-    },
-    isOpen: true
-}
+// let storeName = 'Rozetka';
+// const storeDescription = {
+//     budget: 10000,
+//     employees: ['Anton', 'Valeria', 'Alex'],
+//     products: {
+//         laptop: 100,
+//         mouse: 30
+//     },
+//     isOpen: true
+// }
 
-console.log(storeDescription.employees[1])
+// console.log(storeDescription.employees[1])
+
+
+// const stockholm = {
+//     budget: 1500000,
+//     mainStreet: 'Vasagatan',
+//     isBiggestCity: true,
+//     bestHotels: {
+//         scandic: 100,
+//         blueLaguna: 150,
+//         elite: 250,
+//     }
+// };
+
+// let name1;
+// let data = [1,'black', true, null, name1];
+
+// alert('good!');
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = prompt('Скільки вам років?', '18')
+// console.log(answer);
+
+// const answers = [];
+// answers[0] = prompt('Your Name?', '');
+// answers[1] = prompt('Your age', '');
+// answers[3] = prompt('Your weight?', '');
+
+// console.log(answers);
+
+// const user = 'Nataliia';
+// alert(`Hello ${user}`);
+
+// n = n+1 / n = n++  nt
+
